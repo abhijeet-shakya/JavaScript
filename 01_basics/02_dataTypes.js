4216c399858caef7bfdcd993cdd2d0c0b628ea50
Abhijeet Shakya <120585330+abhijeet-shakya@users.noreply.github.com>
2024-01-04 03:48:52 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+//alert( 3 + 3 ) //we are using nodejs , not browser
+
+console.log("Abhijeet")
+
+console.log(20)
+
+let name = "Abhijeet"
+let age = 19;
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => "Abhijeet"
+// boolean => true/false
+// null => standalone value
+// undefined => declare value but not assign not any value
+// symbol => unique
+
+// 
+
+console.log(typeof "abhijeet")
+console.log(typeof null) // Object
+console.log(typeof undefined) // undefined
+
+
